3f75b62427106d46a4880098a5b7b9b93da9a054
Joshua Tan <jtsarvida@gmail.com>
2018-07-03 11:07:40 -0400

    Following avi in his video, I started making the cli file, note that I'm currently not sure if it would work with correct data, or even fake data, certain elements I'm unceratin of.  These would be if I could run a basketball_game method in another method.  Will reread on subject

--- lib/basketball_simulator_cli_project/player.rb
+++ lib/basketball_simulator_cli_project/player.rb
@@ -1,3 +1,4 @@
 class BasketballSimulator::Player
-
+    attr_accessor :name, :height_and_weight, :team, :birthday_birthplace, :points, :rebounds, :assists, :steals, :blocks, :field_goal, :threepoint, :freethrow
+    
 end
